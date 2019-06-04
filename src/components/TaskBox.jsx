c92a87fa6d75448caceca2697d17f3a886437cde
Danny <caddickdaniel93@gmail.com>
2019-06-04 15:56:01 +0100

    trying to sort taskbox layout

--- src/components/TaskBox.jsx
+++ src/components/TaskBox.jsx
@@ -18,28 +18,31 @@ export default class TaskBox extends Component {
               <input type="textarea" placeholder="Process Description" />
             </div>
           </div>
-        </div>
-        <div className="processBox">
-          <div className="workersBox">
-            <p>Workers Assigned</p>
-            <i class="fas fa-minus-circle" />
-            0/25
-            <i class="fas fa-plus-circle" />
-          </div>
-          <div className="priorityBox">
-            <i class="far fa-star" />
-            <i class="far fa-star" />
-            <i class="far fa-star" />
-          </div>
-          <div className="dropdown">
-            <button
-              class="dropdown-toggle"
-              type="button"
-              data-toggle="dropdown"
-            >
-              <i class="fas fa-cog" />
-            </button>{' '}
-            Unassigned
+
+          <div className="processBox">
+            <div className="workersBox">
+              <p>Workers Assigned</p>
+              <i class="fas fa-minus-circle" />
+              0/25
+              <i class="fas fa-plus-circle" />
+            </div>
+            <div className="priorityBox">
+              <i class="far fa-star" />
+              <i class="far fa-star" />
+              <i class="far fa-star" />
+            </div>
+            <div className="dropdown">
+              <button>
+                <i class="fas fa-cog" />
+                Unassigned
+              </button>
+              <div className="dropdown-content">
+                <p>Auto-Assign</p>
+                <p>Select Workers</p>
+                <p>Schedule</p>
+                <p>Archive</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
