2899c2db69a5b5e240aa32b44ebaa54c201b0e2e
Danny <caddickdaniel93@gmail.com>
2019-06-04 13:26:56 +0100

    sorting app layout

---
+++ src/components/TaskBox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TaskBox extends Component {
+  render() {
+    return (
+      <div className="taskBox">
+        <h1>TaskBox</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/TaskPage.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import TaskBox from '../components/TaskBox';
+
+export default class TaskPage extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <TaskBox />
+        <Footer />
+      </div>
+    );
+  }
+}
